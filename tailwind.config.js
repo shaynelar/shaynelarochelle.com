7464ffeb5441c3bcd80e68bf9db3906900616614
lockedNLevered <shaynel@sfu.ca>
2021-11-29 01:24:23 -0800

    Configure tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-	purge: false,
+	purge: {
+		content: ["./pages/**/*.tsx", "./components/*.tsx"],
+		safelist: ["bg-red-500", "bg-blue-500"], //These styles are not present in
+		//html at bulid time so they must be safelisted
+	},
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {
