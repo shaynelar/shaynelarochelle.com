22214f685461138c0a08a66c2ba453918f3a2379
lockedNLevered <shaynel@sfu.ca>
2021-11-29 17:54:11 -0800

    animate hero text into view

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,19 +1,43 @@
 import Layout from "./Layout";
 import Heading from "./Heading";
+import { motion } from "framer-motion";
 
 function HeroSection() {
 	return (
 		<Layout id="home" as="section" className="hero bg-primary p-4 sm:p-8">
 			<div className="flex flex-col">
-				<Heading className="xl:text-8xl lg:text-7xl md:text-6xl text-5xl font-bold text-blue-100 my-4 lg:my-8 xl:my-12 text-white">
-					<span className="text-blue-500">Shayne</span>
-					<span className="text-fuchsia-500"> LaRochelle</span>
-				</Heading>
-				<p className="text-white md:text-xl lg:text-2xl text-lg mt-2 mb-10 font-semibold md:max-w-3xl text-gray-300">
+				<motion.div
+					initial={{ opacity: 0, x: -200 }}
+					whileInView={{ opacity: 1, x: 0 }}
+					viewport={{ once: true }}
+					transition={{
+						x: {
+							type: "just",
+							stiffness: 1000,
+							velocity: 0.2,
+							ease: "easeOut",
+						},
+						default: { duration: 1.5 },
+					}}
+				>
+					<Heading className="xl:text-8xl lg:text-7xl md:text-6xl text-5xl font-bold text-blue-100 my-4 lg:my-8 xl:my-12 text-white">
+						<span className="text-blue-500">Shayne</span>
+						<span className="text-fuchsia-500"> LaRochelle</span>
+					</Heading>
+				</motion.div>
+				<motion.p
+					initial={{ opacity: 0 }}
+					whileInView={{ opacity: 1 }}
+					viewport={{ once: true }}
+					transition={{
+						default: { duration: 2.2, delay: 0.4 },
+					}}
+					className="text-white md:text-xl lg:text-2xl text-lg mt-2 mb-10 font-semibold md:max-w-3xl text-gray-300"
+				>
 					I&apos;m a Professional Golfer turned Software Developer with
 					experience designing, developing and deploying full-stack
 					applications.
-				</p>
+				</motion.p>
 			</div>
 		</Layout>
 	);
