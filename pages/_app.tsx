7a80997fa0de14c609f99802ce52b86eadb6d127
lockedNLevered <shaynel@sfu.ca>
2022-01-05 13:30:00 -0800

    convert to reducer to handle localstorage setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,19 +7,43 @@ import {
 	SetStateAction,
 	useContext,
 	useEffect,
+	useReducer,
 	useState,
-	useLayoutEffect
+	useLayoutEffect,
 } from "react";
 
 interface Theme {
-	isDark: boolean;
-	setIsDark: Dispatch<SetStateAction<boolean>>;
+	state: {
+		isDark: boolean;
+	};
+
+	setIsDark: Dispatch<ActionType>;
 }
 
 const initialTheme = {
-	isDark: false,
-	setIsDark: () => null as unknown as Dispatch<SetStateAction<boolean>>,
+	state: {
+		isDark: false,
+	},
+	setIsDark: () => null,
 };
+
+type ActionType =
+	| { type: "TOGGLETHEME" }
+	| { type: "INITIALIZE"; payload: boolean };
+
+function themeReducer(state: boolean, action: ActionType): boolean {
+	switch (action.type) {
+		case "INITIALIZE":
+			localStorage.setItem("isDark", JSON.stringify(action.payload));
+			return action.payload;
+
+		case "TOGGLETHEME":
+			const currentState = !state;
+			localStorage.setItem("isDark", JSON.stringify(currentState));
+			return currentState;
+	}
+}
+
 export const ThemeContext = createContext<Theme>(initialTheme);
 
 function handleTheme(state: boolean) {
@@ -27,17 +51,22 @@ function handleTheme(state: boolean) {
 }
 function MyApp({ Component, pageProps }: AppProps) {
 	const [isDark, setIsDark] = useState(true);
+	const [state, dispatch] = useReducer(themeReducer, false);
 
 	useLayoutEffect(() => {
 		if (localStorage.getItem("isDark")) {
-			setIsDark(JSON.parse(localStorage.getItem("isDark") as string));
+			dispatch({
+				type: "INITIALIZE",
+				payload: JSON.parse(
+					localStorage.getItem("isDark") as string
+				) as boolean,
+			});
 		}
-
 		const html = document.querySelector("html");
 		isDark ? html?.classList.add("dark") : html?.classList.remove("dark");
 	}, [isDark]);
 	return (
-		<ThemeContext.Provider value={{ isDark, setIsDark }}>
+		<ThemeContext.Provider value={{ state, disatch }}>
 			<Component {...pageProps} />
 		</ThemeContext.Provider>
 	);
